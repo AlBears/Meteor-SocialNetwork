125e74d78a04954561e9e221d41e3461580221f0
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-18 17:54:05 +0300

    routes access

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -5,21 +5,37 @@ FlowRouter.route('/', {
   }
 });
 
+//check if user logged in to restrict access to signup and login
+let loggedCheck = [
+  () => { if (Meteor.userId()){FlowRouter.go('dashboard')}}
+];
+
 FlowRouter.route('/signup', {
   name: 'signup',
   action () {
     BlazeLayout.render('fullLayout', {main: 'signup'});
-  }
+  },
+  triggersEnter: loggedCheck
 });
 
 FlowRouter.route('/login', {
   name: 'login',
   action () {
     BlazeLayout.render('fullLayout', {main: 'login'});
-  }
+  },
+  triggersEnter: loggedCheck
 });
+//user group -> we restrict access for not registered
+let userRoutes = FlowRouter.group({
+  prefix: '/user',
+  name: 'userRoutes',
+  triggersEnter: [
+    () => { if (!Meteor.userId()){FlowRouter.go('home')}}
+  ]
+})
 
-FlowRouter.route('/dashboard', {
+//Dashboard route
+userRoutes.route('/dashboard', {
   name: 'dashboard',
   action () {
     BlazeLayout.render('mainLayout', {
